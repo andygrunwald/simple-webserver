9622a4173a973da7991b72462d8138401833089a
Andy Grunwald <andygrunwald@gmail.com>
2015-11-27 20:00:29 +0100

    Fix #2: Add version endpoint

--- main.go
+++ main.go
@@ -7,15 +7,22 @@ import (
 	"net/http"
 )
 
+const (
+	Name = "simple webserver"
+	Version = "0.1.0-dev"
+)
+
 func main() {
 	var (
 		listen = flag.String("listen", ":8082", "Address + Port to listen on. Format ip:port.")
 	)
 	flag.Parse()
 
-	log.Printf("Starting webserver and listen on %s", *listen)
 	http.HandleFunc("/", httpHandleRoot)
 	http.HandleFunc("/ping", httpHandlePing)
+	http.HandleFunc("/version", VersionHandler)
+
+	log.Printf("Starting webserver and listen on %s", *listen)
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
 
@@ -26,4 +33,9 @@ func httpHandleRoot(resp http.ResponseWriter, req *http.Request) {
 func httpHandlePing(resp http.ResponseWriter, req *http.Request) {
 	resp.WriteHeader(http.StatusOK)
 	fmt.Fprintln(resp, "pong")
+}
+
+func VersionHandler(resp http.ResponseWriter, req *http.Request) {
+	resp.WriteHeader(http.StatusOK)
+	fmt.Fprintf(resp, "%s v%s", Name, Version)
 }
