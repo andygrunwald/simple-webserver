53f5d80d9df5ecc37310a3018cc71f01986bcccd
Andy Grunwald <andygrunwald@gmail.com>
2016-06-11 19:49:42 +0200

    Added godoc

---
+++ doc.go
@@ -0,0 +1,27 @@
+// Simple webserver
+//
+// A small webserver for testing various technologies, techniques and concepts.
+//
+// This project enables you to learn new technologies by try them in
+// a small and simple environment to get a first idea how things are working.
+//
+// This part is responsible to show the standard way of documentation
+// of golang. With the idea to inline the documentation, code and docs are one.
+// This minimize the risk that documentation get out of date quickly.
+//
+// Notice that this is only a a small and not production ready demo.
+// If you want to deep dive into the topic of golang + documentation
+// i suggest to checkout
+//
+// * https://blog.golang.org/godoc-documenting-go-code
+// * https://godoc.org/golang.org/x/tools/cmd/godoc
+// * https://github.com/fluhus/godoc-tricks
+// * https://github.com/golang/gddo
+//
+// If you have any suggestion or comment, please feel free to open an issue on
+// this the GitHub page of this project!
+//
+// More information and details can be found there as well.
+// Checkout https://github.com/andygrunwald/simple-webserver
+//
+package main
