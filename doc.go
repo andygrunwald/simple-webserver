b1c3935f859c64e7b72885e58b9c3e85e09e973c
Andy Grunwald <andygrunwald@gmail.com>
2016-06-11 19:51:54 +0200

    Make points more readable

--- doc.go
+++ doc.go
@@ -11,15 +11,15 @@
 //
 // Notice that this is only a a small and not production ready demo.
 // If you want to deep dive into the topic of golang + documentation
-// i suggest to checkout
+// i suggest to checkout:
 //
-// * https://blog.golang.org/godoc-documenting-go-code
+// https://blog.golang.org/godoc-documenting-go-code
 //
-// * https://godoc.org/golang.org/x/tools/cmd/godoc
+// https://godoc.org/golang.org/x/tools/cmd/godoc
 //
-// * https://github.com/fluhus/godoc-tricks
+// https://github.com/fluhus/godoc-tricks
 //
-// * https://github.com/golang/gddo
+// https://github.com/golang/gddo
 //
 // If you have any suggestion or comment, please feel free to open an issue on
 // this the GitHub page of this project!
